e59f8aa5939360ab648710d2225f44f07e8257fc
saktiprasad-sahu <saktiprasadsahu8@gmail.com>
2025-07-02 20:19:53 +0530

    Initial commit with dataset and app

---
+++ AttentionLayer.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+
+class Attention(Layer):
+    def __init__(self, **kwargs):
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.W = self.add_weight(name='attention_weight', shape=(input_shape[-1], 1),
+                                 initializer='random_normal', trainable=True)
+        self.b = self.add_weight(name='attention_bias', shape=(input_shape[1], 1),
+                                 initializer='zeros', trainable=True)
+        super(Attention, self).build(input_shape)
+
+    def call(self, inputs):
+        e = K.tanh(K.dot(inputs, self.W) + self.b)
+        a = K.softmax(e, axis=1)
+        output = inputs * a
+        return K.sum(output, axis=1)
